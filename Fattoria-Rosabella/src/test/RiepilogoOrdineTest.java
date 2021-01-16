4e0e19c0aca4b7d5f5ea760c554f10b2acc7db64
Carmine Pastore <39345@MSI>
2021-01-16 19:16:36 +0100

    testing r.o.

--- Fattoria-Rosabella/src/test/RiepilogoOrdineTest.java
+++ Fattoria-Rosabella/src/test/RiepilogoOrdineTest.java
@@ -1,5 +1,26 @@
 package test;
 
-public class RiepilogoOrdineTest {
+import beans.RiepilogoOrdine;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class RiepilogoOrdineTest extends TestCase{
+	private RiepilogoOrdine ro;
+	
+	@Override
+	protected void setUp() throws Exception {
+		ro = new RiepilogoOrdine(777);
+	}
+	
+	public void testSetId_R() {
+		ro.setId_riepilogo(187);
+		assertEquals(187, ro.getId_riepilogo());
+	}
+	
+	public static Test suite() {
+		return new TestSuite(RiepilogoOrdineTest.class);
+		
+	}
 
 }
