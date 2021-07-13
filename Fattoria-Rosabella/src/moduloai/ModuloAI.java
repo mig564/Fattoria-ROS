8047b8173e2563100d7a6ef8707f4271b2bd0cf9
mig564 <pmignoli@outlook.com>
2021-07-13 16:22:10 +0200

    correzioni bug

--- Fattoria-Rosabella/src/moduloai/ModuloAI.java
+++ Fattoria-Rosabella/src/moduloai/ModuloAI.java
@@ -24,27 +24,36 @@ public class ModuloAI {
 	
 	private static Assegnamento backtracking(Assegnamento assegnamento, int bambino2, int tipologia, int min, int max) {
 		
-		if(assegnamento.isCompleto()) return assegnamento;
-		
-		int var = assegnamento.getVariabile(); // Scegliamo la variabile pi� piccola non assegnata (Passo 1: bambino)
-		
-		if(var == 0) i = dominio.getBambino();
-		else if(var == 1) i = dominio.getTipo();
-		else if(var == 2) i = dominio.getMeteo();
-		else if(var == 3) i = dominio.getPartecipanti();
-
-		for(Integer valore : i) { // Per ogni valore (true o false) assegnamo un valore (Passo 2: bambino = true)
-			if(assegnamentoConsistente(var, valore,  bambino2, tipologia, min, max)) { // Controllo se assegnamento � consistente (Passo 3: utente ha messo si, dunque per bambino = true assegnamento consistente)
-				assegnamento.setValore(var); // Aggiungi ad attivit�
-				assegnamento = backtracking(assegnamento, bambino2, tipologia, min, max); // Richiamiamo backtracking
-			}
-			if(assegnamento.isCompleto()) return assegnamento;	
-		}
-		return assegnamento;
 	}
 
 	private static boolean assegnamentoConsistente(int var, int valore, int bambino2, int tipologia, int min, int max) {
-		
+		if(var == 0) {
+			if ((valore == bambino2) && bambino2 == 0) return true; 
+			else if ((valore == bambino2) && bambino2 == 1) {
+				if (dominio.getAttivita().getCategoria().equals("Visita guidata") || dominio.getAttivita().getCategoria().equals("Ristoro")) 
+					return true;
+			}
+		}
+		if(var == 1) {
+			if((valore == tipologia) && tipologia == 0) {
+				if (dominio.getAttivita().getCategoria().equals("Balneazione"))
+					return true;
+			}
+			else if((valore == tipologia) && tipologia == 1) {
+				if (dominio.getAttivita().getCategoria().equals("Escursione"))
+					return true;
+			}
+			else if((valore == tipologia) && tipologia == 2) {
+				if (dominio.getAttivita().getCategoria().equals("Visita guidata"))
+					return true;
+			}
+		}
+		if(var == 2) {
+			return true; //Aggiungere check meteo
+		}
+		if(var == 3) {
+			if(min <= valore && max >= valore) return true;
+		}
 		return false;
 	}
 
